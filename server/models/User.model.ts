a0d51a7768bfe6b59c883d13e87c2ff2f7eaef83
Harish Soni <soniharish029@gmail.com>
2026-07-12 21:40:57 +0530

    added user model

---
+++ server/models/User.model.ts
@@ -0,0 +1,45 @@
+import  { Schema,model } from "mongoose";
+
+enum Gender{
+    MALE="male",
+    FEMALE="female",
+    OTHER="other"
+}
+
+interface IUser{
+    fullName: string
+    password: string
+    email: string
+    gender: Gender
+}
+
+const userSchema  = new Schema<IUser>({
+    fullName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    gender: {
+        type: String,
+        required: [true, "Gender is required"],
+        enum: Object.values(Gender)
+    }
+},{
+    timestamps:true
+})
+
+userSchema.index({ email: 1 });
+
+const User = model<IUser>("User", userSchema)
+
+export default User
