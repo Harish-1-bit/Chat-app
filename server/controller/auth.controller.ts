4c8ab1493aa5bdc7bea86764359c50f93c2f9b5a
Harish Soni <soniharish029@gmail.com>
2026-07-13 00:05:10 +0530

    created a controlle for auth

---
+++ server/controller/auth.controller.ts
@@ -0,0 +1,14 @@
+// User login
+const userLogin = async (req, res) => {
+    res.send("Hello from user login")
+}
+
+// User Sign Up
+const userSignup = async (req, res) => {
+    res.send("Hello from user signup")
+}
+
+// User Logout
+const userLogout = async (req, res) => {
+    res.send("Hello from user logout")
+}
