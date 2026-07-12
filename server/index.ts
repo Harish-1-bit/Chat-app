c7c3347691e976135dd7a546516b83c0a6dd0ae9
Harish Soni <soniharish029@gmail.com>
2026-07-12 21:07:05 +0530

    first commit of chat app

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+dotenv.config()
+const app = express()
+
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }))   
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`)
+})
