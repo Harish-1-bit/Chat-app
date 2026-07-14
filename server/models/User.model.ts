5a30667f2389e125287ce73c53fcced9de6aa7ad
Harish Soni <soniharish029@gmail.com>
2026-07-14 23:31:23 +0530

    removed the double indexing from the user model

--- server/models/User.model.ts
+++ server/models/User.model.ts
@@ -45,6 +45,6 @@ const userSchema  = new Schema<IUser>({
     timestamps:true
 })
 
-// const User = model<IUser>("User", userSchema)
+const User = model<IUser>("User", userSchema)
 
 export default User
