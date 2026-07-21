1ad518a23df9f47dd96dfe9f8c34919edce40aa7
Harish Soni <soniharish029@gmail.com>
2026-07-21 23:22:58 +0530

    check the

--- server/index.ts
+++ server/index.ts
@@ -23,7 +23,7 @@
       cors: { origin: "http://localhost:5173", methods: ["GET", "POST"],credentials:true },
     })
 
-  //  setupSocket(io)
+   setupSocket(io)
 
   // test api
   app.get("/", (req, res) => {
